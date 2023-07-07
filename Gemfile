034c04104f83a7a6964bf3c614da097d2f5d076e
yosyncr <yizhig866@gmail.com>
2023-07-07 15:33:34 +0900

    ログイン、ログアウト、バリデーションの設定etc

--- Gemfile
+++ Gemfile
@@ -80,3 +80,4 @@ group :production do
 end
 
 gem 'devise'
+gem 'pry-rails'
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         validates :nickname, presence: true
+         validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/ }
+         validates :password, presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/ }
+         validates :password_confirmation, presence: true
+         validates :last_name,:first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+         validates :last_name_kana,:first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+         validates :birthdate, presence: true
 end
