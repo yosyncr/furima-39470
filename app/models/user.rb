64ffa94366dbf079d2b1bd8e39c00892f6311a45
yosyncr <yizhig866@gmail.com>
2023-07-08 17:46:26 +0900

    テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
          validates :nickname, presence: true
-         validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/ }
+         validates :email, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/ }
          validates :password, presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/ }
-         validates :password_confirmation, presence: true
-         validates :last_name,:first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
-         validates :last_name_kana,:first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-         validates :birthdate, presence: true
+         validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+         validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+         validates :kana_first_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+         validates :kana_last_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+         validates :birth_date, presence: true
 end
