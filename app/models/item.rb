e03337025fd776ec517991a0c78401ee2d7a084c
yosyncr <yizhig866@gmail.com>
2023-07-13 03:21:20 +0900

    バリデーションとテストコードの再記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,23 +1,27 @@
 class Item < ApplicationRecord
   belongs_to :user
   extend ActiveHash::Associations::ActiveRecordExtensions
-  # belongs_to :category
-  #  belongs_to :condition
-  #   belongs_to :fee
-  #   belongs_to :region
-  #   belongs_to :delivery
+  belongs_to :category
+   belongs_to :condition
+    belongs_to :fee
+    belongs_to :region
+    belongs_to :delivery
+  validates :item_name, presence: true
   validates :category_id, presence: true
-  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :description,presence: true
+  validates :image, presence: true
   validates :condition_id, presence: true
-  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :fee_id, presence: true
-  validates :fee_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :region_id, presence: true
-  validates :region_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :delivery_id, presence: true
+  validates :price, presence: true,format:{with: /\A[0-9]+\z/}
+  validates :region_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :delivery_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :fee_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "must be between ¥300 and ¥9,999,999" }
   has_one_attached :image
-  validates :image, presence: true
+ 
 
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Item, type: :model do
   before do
     @item = FactoryBot.build(:item)
   end
-  describe 'ユーザー新規登録' do
+  describe '出品できない場合' do
   it 'カテゴリーがないと出品できない'do
   @item.category_id = ''
   @item.valid?
@@ -50,11 +50,45 @@ RSpec.describe Item, type: :model do
     @item.valid?
     expect(@item.errors.full_messages).to include("Category can't be blank")
   end
+  it 'condition_idが1以外でなければ出品できない' do
+    @item.condition_id = 1
+    @item.valid?
+    expect(@item.errors.full_messages).to include("Condition can't be blank")
+  end
+  it 'fee_idが1以外でなければ出品できない' do
+    @item.fee_id = 1
+    @item.valid?
+    expect(@item.errors.full_messages).to include("Fee can't be blank")
+  end
+  it 'region_idが1以外でなければ出品できない' do
+    @item.region_id = 1
+    @item.valid?
+    expect(@item.errors.full_messages).to include("Region can't be blank")
+  end
+  it 'delivery_idが1以外でなければ出品できない' do
+    @item.category_id = 1
+    @item.valid?
+    expect(@item.errors.full_messages).to include("Category can't be blank")
+  end
   it '画像がないと出品出来ない'do
-    @item.image= nil
+    @item.image = nil
     @item.valid?
     expect(@item.errors.full_messages).to include("Image can't be blank")
   end
-
+  it '値段が300円以下では出品できない' do
+    @item.price= 1
+    @item.valid?
+    expect(@item.errors.full_messages).to include("Price must be between ¥300 and ¥9,999,999")
+  end
+  it '値段が9999999円より高いと出品出来ない' do
+    @item.price= 111111111111111111
+    @item.valid?
+    expect(@item.errors.full_messages).to include("Price must be between ¥300 and ¥9,999,999")
+  end
  end
+ describe '出品できる場合' do
+  it 'すべての情報が入力できている場合'do
+  expect(@item).to be_valid
+ end
+end
 end
