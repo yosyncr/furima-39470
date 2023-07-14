a17e6c194d9df77425defc8ad7d6e58a66ef4bea
yosyncr <yizhig866@gmail.com>
2023-07-14 18:03:12 +0900

    再修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,9 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index,:show]
-  before_action :set_item,only: [:show,:edit,:update]
+  before_action :set_item,only: [:show,:edit,:update,:destroy]
+  before_action :contributor_confirmation, only:[:edit,:destroy]
+
+
   def index
     @items = Item.includes(:user).order("created_at DESC")
   end
@@ -22,9 +25,6 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    unless @item.user == current_user
-      redirect_to root_path
-    end
   end
 
   def update
@@ -36,7 +36,6 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item = Item.find(params[:id])
     if @item.destroy
       redirect_to root_path
     else
@@ -54,4 +53,9 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def contributor_confirmation
+    unless @item.user == current_user
+      redirect_to root_path
+    end
+  end
 end
